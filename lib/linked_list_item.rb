252a9e8fc58f5db7edcba27987d088f51c336f5e
averygoodplanet <averygoodplanet@gmail.com>
2013-12-12 09:43:38 -0600

    comments from Brandon tutoring Thurs morning

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,17 +1,36 @@
 class LinkedListItem
+#single-line comment in ruby starts with pound sign
+
+# attr_reader :payload  creates function like
+#     def payload
+#        @payload
+#     end
+# this function lets you get the value of
+# instance-variable payload
+
+#attr_writer :something creates function like
+#   def something=(value)
+#      @something=value
+#  end
+#this function sets the value of
+# variable something
+# NOTE: in ruby the equal sign is part of the method name, and is different from a method
+# name without equal sign
+
+# attr_accessor :apples creates  both attr_writer and attr_reader functions for apples
+
+#you can do a series e.g. attr_accessor :apples, :oranges,
+
+# by default a ruby method returns the value of the last expression,  or the last condition
 
   def initialize (payload)
-    # comment ruby with pound sign
-    @payload = payload
-  end
-
-  def payload
-    # by default a ruby method returns the value of the last expression
-    @payload
-  end
-
-  def next_list_item(nextItem)
-    #  ***Need your 6hrs sleep (it's midnight, you setup your native environment, resolved subl etc... work on this tomorrow)
+      #initialize function creates the object and sets instance variables
+      @payload = payload
   end
 end
+
+# LinkedListItem.new("foo").payload => "foo"   #creates a new object with value "foo"
+# then gets "foo" value
+
+#LinkedListItem.new.something = "bar"
 

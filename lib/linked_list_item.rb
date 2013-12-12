1bb25a7e94dae5067261d121ed253a4429a15433
averygoodplanet <averygoodplanet@gmail.com>
2013-12-12 00:02:20 -0600

    on 4/63 going to bed

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -9,5 +9,9 @@ class LinkedListItem
     # by default a ruby method returns the value of the last expression
     @payload
   end
+
+  def next_list_item(nextItem)
+    #  ***Need your 6hrs sleep (it's midnight, you setup your native environment, resolved subl etc... work on this tomorrow)
+  end
 end
 

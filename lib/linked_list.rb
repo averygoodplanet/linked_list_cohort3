39ae12b27a80c28856cde28ee1fd0f826f3c7582
averygoodplanet <averygoodplanet@gmail.com>
2013-12-14 12:52:20 -0600

    on 06c

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,8 +8,13 @@ class LinkedList
     #if first_node is not yet a LinkedListItem, then
     # create a new LinkListItem(payload) and assign to first_node
     # JUST CREATE A LIST ITEM WITHOUT CHECKING FIRST
+
     lli = LinkedListItem.new(payload)
-    @first_node=lli
+    if @first_node.nil?
+      @first_node=lli
+    else
+
+    end
     #if first_node is already a LinkedListItem, then
     # find the last item, and add a next item to it? e.g. next_list_item=(value)
   end
